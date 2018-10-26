2289a2d38986f132614473d78037c3308461bae1
JEREMY CHEN <jc587@duke.edu>
2018-10-26 01:27:37 -0400

    react navigation added successfully '

--- App.js
+++ App.js
@@ -1,54 +1,53 @@
-import React, { Component } from 'react';
-import { Container, Content } from 'native-base';
-import { StyleSheet } from 'react-native';
-import { Font } from 'expo';
-import Start from './components/Start';
+import React, { Component } from "react";
+import { Container, Content } from "native-base";
+import { StyleSheet } from "react-native";
+import { Font } from "expo";
+import Start from "./components/Start";
+import { createStackNavigator } from "react-navigation";
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      fontLoaded: false,
-    }
-
+      fontLoaded: false
+    };
   }
 
   async componentDidMount() {
     try {
       await Font.loadAsync({
-        'Avenir-Light': require('./assets/fonts/AvenirLTStd-Light.otf'),
-        'Glacial': require('./assets/fonts/GlacialIndifference-Regular.otf'),
-      })
+        "Avenir-Light": require("./assets/fonts/AvenirLTStd-Light.otf"),
+        Glacial: require("./assets/fonts/GlacialIndifference-Regular.otf")
+      });
       this.setState({ fontLoaded: true });
-      console.log('fonts are loaded')
+      console.log("fonts are loaded");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 
   render() {
     const { fontLoaded } = this.state;
     return (
+      <RootStack />
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: HomeScreen,
+  // OAuth: OAuthScreen,
+  // Choose: ChooseScreen,
+});
+
+class HomeScreen extends Component {
+  render() {
+    return (
       <Container>
         <Content>
-          {
-            fontLoaded ? (
-              <Start />
-            ) : null
-          }
-
+          <Start />
         </Content>
       </Container>
     );
   }
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
