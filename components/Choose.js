15f386cf3e4ae9648721942fd26c90cfa9d93d9b
Lasia Lo <lasia.lo@duke.edu>
2018-10-26 16:03:51 -0400

    Refactoring

--- components/Choose.js
+++ components/Choose.js
@@ -1,51 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { Text } from "react-native";
 import { Container, Content } from "native-base";
 import { Row, Grid } from "react-native-easy-grid";
-import { Button } from "native-base";
 import style from "../style/style";
+import QButton from './reuse/QButton';
 import { createStackNavigator } from "react-navigation";
+import Start from "./Start";
 
-export default class Choose extends Component{
-  static navigationOptions = {
-    
-  };
 
-  constructor(props) {
-    super(props);
-  }
+const Choose = ({ navigation }) => (
+  <Container>
+    <Content>
+      <Grid style={style.fs}>
+        <Row style={style.centerRow} size={1.75}>
+          <Text style={style.letsGo}> Let's go! </Text>
+        </Row>
+        <Row style={style.titleRow} size={1} />
+        <Row
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignContent: "center"
+          }}
+          size={1.75}
+        >
+          <QButton
+            onPress={() => navigation.navigate("Home")}
+            type='green'
+          >
+            go BACK
+          </QButton>
+        </Row>
+      </Grid>
+    </Content>
+  </Container>
+);
 
-  render() {
-    return (
-      <Container>
-        <Content>
-          <Grid style={style.fs}>
-            <Row style={style.centerRow} size={1.75}>
-              <Text style={style.letsGo}> Let's go! </Text>
-            </Row>
-            <Row style={style.titleRow} size={1} />
-            <Row
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignContent: "center"
-              }}
-              size={1.75}
-            >
-              <Button
-                onPress={() => this.props.navigation.navigate("Home")}
-                rounded
-                success
-                block
-                style={style.greenBtn}
-              >
-                <Text style={style.greenBtnText}>go BACK</Text>
-              </Button>
-            </Row>
-          </Grid>
-        </Content>
-      </Container>
-    );
-  }
-}
+Choose.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
+
+export default Choose;
+
+
 
